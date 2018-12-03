dc2509abc2c1f06ae8ea67194e07067377cebaad
Pedro Lucas Silva <pedrolsilva00@gmail.com>
2018-12-02 23:51:23 -0200

    adding update changes

--- Organizer/src/java/br/cefetmg/inf/organizer/controller/UpdateTag.java
+++ Organizer/src/java/br/cefetmg/inf/organizer/controller/UpdateTag.java
@@ -7,8 +7,10 @@ import br.cefetmg.inf.organizer.model.service.IKeepItem;
 import br.cefetmg.inf.organizer.model.service.IKeepTag;
 import br.cefetmg.inf.organizer.model.service.impl.KeepItem;
 import br.cefetmg.inf.organizer.model.service.impl.KeepTag;
+import br.cefetmg.inf.util.GsonUtil;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,43 +19,26 @@ public class UpdateTag implements GenericProcess {
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
-        String pageJSP = "";
-        List<Item> itemList;
 
-        HttpSession session = req.getSession();
-        User user = (User) session.getAttribute("user");
-        String oldNameTag = req.getParameter("takeOldName");
-        String nameTag = req.getParameter("nameEdited");
+        Map<String, Object> parameterMap = (Map<String, Object>) req.getAttribute("mobile-parameters");
+        String email = (String) parameterMap.get("email");
+        Long seqTag = (Long) parameterMap.get("seqTag");
+        String nameTag = (String) parameterMap.get("nameTag");
+
+        User user = new User();
+        user.setCodEmail(email);
 
         IKeepTag keepTag = new KeepTag();
 
         Tag tag = new Tag();
-        tag.setSeqTag(keepTag.searchTagByName(oldNameTag, user));
+        tag.setSeqTag(seqTag);
         tag.setTagName(nameTag);
         tag.setUser(user);
 
         boolean result = keepTag.updateTag(tag);
 
-        if (result == false) {
-        } else {
-            IKeepItem keepItem = new KeepItem();
-            itemList = keepItem.listAllItem(user);
-            if (itemList == null) {
-                req.setAttribute("itemList", new ArrayList());
-            } else {
-                req.setAttribute("itemList", itemList);
-            }
+        String success = GsonUtil.toJson(result);
 
-            List<Tag> tagList = keepTag.listAlltag(user);
-            if (tagList == null) {
-                req.getSession().setAttribute("tagList", new ArrayList());
-            } else {
-                req.getSession().setAttribute("tagList", tagList);
-            }
-
-            pageJSP = "/index.jsp";
-        }
-
-        return pageJSP;
+        return success;
     }
 }
